ad88ec1fd976f15d4701e4ae75ed47181265aea5
thnhan3 <huunhantran10@gmail.com>
2024-10-08 10:49:39 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Xin chao</h1>
+      <h2>Deploy with github action</h2>
+      <h2 style={{ color: "red" }}>Deploy with github action</h2>
+    </>
+  );
+}
+
+export default App;
